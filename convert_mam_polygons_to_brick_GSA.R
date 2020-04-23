57d7bf5cddb04b5658e30c236a52aea7adc8691a
Gwen Antell <gwenantell@gmail.com>
2020-04-23 13:26:06 +0100

    Update path to spatial_data folder

--- convert_mam_polygons_to_brick_GSA.R
+++ convert_mam_polygons_to_brick_GSA.R
@@ -18,7 +18,7 @@ values(r_prj) <- 1:ncell(r_prj)
 # North American mammal ranges
 
 # Remove species not found within continental US extent
-mam_data <- readOGR("Data/shapefiles/TERRESTRIAL_MAMMALS.shp")
+mam_data <- readOGR("Data/spatial_data/TERRESTRIAL_MAMMALS.shp")
 mam_clipped <- crop(mam_data, extent(us_bounds))
 
 # use exactly the same 342 species as by-species approach
@@ -43,17 +43,17 @@ for (sp_nm in spp) {
 beep('treasure')
 # native 'raster' format will preserve layers names when read back in to R
 # but also export a header file in order to open file in other app, e.g. QGIS
-sp_nm <- paste0('Data/NAm_mammal_ranges_',grid_res,'degree_any_overlap')
+sp_nm <- paste0('Data/spatial_data/NAm_mammal_ranges_',grid_res,'degree_any_overlap')
 writeRaster(spp_brik, sp_nm, format='raster', overwrite=TRUE)
 hdr(spp_brik, filename=sp_nm, format='ENVI')
 
 # Format Paleozoic sediment polygons as a brick:
-all_sed_fls <- list.files('Data/Stage_shp_files', recursive=TRUE)
+all_sed_fls <- list.files('Data/spatial_data', recursive=TRUE)
 shp_fl_pos <- grep('.shp', all_sed_fls)
 xml_fl_pos <- grep('.shp.xml', all_sed_fls)
 shp_fl_pos <- setdiff(shp_fl_pos, xml_fl_pos)
 shp_fls <- all_sed_fls[shp_fl_pos]
-shp_fls_dsn <- paste0('Data/Stage_shp_files/', shp_fls)
+shp_fls_dsn <- paste0('Data/spatial_data/', shp_fls)
 bins <- sapply(shp_fls, function(x) str_split(x, '_')[[1]][2] )
 n_bins <- length(bins)
 sed_brik <- brick()
@@ -68,6 +68,6 @@ for (j in 1:n_bins){
   names(sed_r) <- bins[j]
   sed_brik <- brick(list(sed_brik, sed_r))
 }
-sed_nm <- paste0('Data/stage_sediment_',grid_res,'degree_any_overlap')
+sed_nm <- paste0('Data/spatial_data/stage_sediment_',grid_res,'degree_any_overlap')
 writeRaster(sed_brik, sed_nm, format='raster', overwrite=TRUE)
 hdr(sed_brik, filename=sed_nm, format='ENVI')
